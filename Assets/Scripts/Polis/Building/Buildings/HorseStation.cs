bc8a3dae7bf310a7127f97a915c4bf8f35e8eb74
Nianyi Wang <wangnianyi2001@outlook.com>
2026-01-25 00:26:11 -0500

    搭建驿站 UI

---
+++ Assets/Scripts/Polis/Building/Buildings/HorseStation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace LongLiveKhioyen
+{
+	public class HorseStation : Building
+	{
+		const string uiAssetName = "Horse Station";
+
+		public override IEnumerable<InspectionAction> GetInspectionAction()
+		{
+			yield return new()
+			{
+				text = "Open",
+				action = () => OpenBuildingUiByName(uiAssetName),
+			};
+		}
+
+		public override GameObject CreateInspectionUi()
+		{
+			return GetInspectionUiByName(uiAssetName);
+		}
+	}
+}
